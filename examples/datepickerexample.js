d7b2a1f5d2e16f2515efe7ca3c6546d20df0197b
matkat99 <st83440@gmail.com>
2020-02-06 12:31:43 -0700

    Update datepickerexample.js

--- examples/datepickerexample.js
+++ examples/datepickerexample.js
@@ -1,5 +1,5 @@
-import datePicker from './datePicker.js';
-import { name, course } from './config.js';
+import datePicker from "./datepicker.js";
+import { name, course } from "./config.js";
 
-datePicker('.myDate', 'Enter Your Birthday');
+datePicker(".myDate", "Enter Your Birthday");
 console.log(name, course);
