2bd6b0f405bf5f7eda1466a3d1f33b3513319b3e
matkat99 <st83440@gmail.com>
2020-11-11 10:31:04 -0700

    new team1 activity

--- solutions/week05/app5.js
+++ solutions/week05/app5.js
@@ -1,6 +1,7 @@
-import Hikes from './hikes.js';
+import Hikes from "./hikes.js";
 //on load grab the array and insert it into the page
-const myHikes = new Hikes('hikes');
-window.addEventListener('load', () => {
+const myHikes = new Hikes("hikes");
+window.addEventListener("load", () => {
   myHikes.showHikeList();
 });
+myHikes.hikeList;
